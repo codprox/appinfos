9c889d780378ebe4736e65ab193dfe8b69e62fa3
ArmandMOUELE <armandmoueleprofessionnel@gmail.com>
2024-08-01 07:41:00 +0100

    update

--- design/js/app.js
+++ design/js/app.js
@@ -60,17 +60,3 @@ apps.controller('rootCtrl', function($scope,PostService){
 		$scope.changeState_();
 	})
 })
-
-// var dir = "/videos";
-// var fileextension = ".mp4";
-// $.ajax({
-//     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-//     url: dir,
-//     success: function (data) {
-//         // List all mp4 file names in the page
-//         $(data).find("a:contains(" + fileextension + ")").each(function () {
-//             var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-//             $("body").append($("<img src=" + dir + filename + "></img>"));
-//         });
-//     }
-// });
